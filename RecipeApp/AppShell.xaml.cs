d0fe638c0be3402139753c0651a9166b582e53ab
A00218636 <84596063+A00218636@users.noreply.github.com>
2021-10-10 19:36:44 -0700

    Initial Commit with Sign up page
    Initial Commit with Sign up page

---
+++ RecipeApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using RecipeApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RecipeApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(SignUpPage), typeof(SignUpPage));
+            
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
